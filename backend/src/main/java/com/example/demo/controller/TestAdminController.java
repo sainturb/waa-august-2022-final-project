cabf093f6a9b04e059ad457d1c98f0fdc29af68b
sainturb <saintur.batkhuu@miu.edu>
2022-08-02 19:47:07 -0500

    backend keycloak adapter added

---
+++ backend/src/main/java/com/example/demo/controller/TestAdminController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequiredArgsConstructor
+@RequestMapping("api")
+public class TestController {
+
+    @GetMapping("test")
+    public String getTest() {
+        return "Completed";
+    }
+}
