f817969940735712de95cde734b0fbda75b5404b
amgalanbe <amgalanbe@gmail.com>
2022-08-07 13:34:45 -0500

    implemented Job ad frontend

--- frontend/src/private/ads/Ads.js
+++ frontend/src/private/ads/Ads.js
@@ -3,33 +3,145 @@ import axios from "axios";
 
 function Ads () {
     const [ads, setAds] = useState([]);
+    const [query, setQuery] = useState('');
+    const [filter, setFilter] = useState({state: null, city: null, company: null, tags: null });
+
     const fetch = () => {
-        axios.get(`/api/advertisements`).then(response => {
+        axios.get('/api/advertisements').then(response => {
             if (response.data) {
                 setAds(response.data);
             }
         });
+    };
+
+    const fetchQuery = () => {
+        axios.get('/api/advertisements/search', {params: {query}}).then(response => {
+            if(response.data){
+                setAds(response.data);
+            }
+        });
+    };
+
+    const fetchFilter = () => {
+        const params = {};
+        Object.keys(filter).forEach(key => {
+            if(filter[key]) {
+                params[key] = filter[key];
+            }
+        });
+
+        axios.get('/api/advertisements/filter', {params: params}).then(response => {
+            console.log(response);
+            if(response.data) {
+                setAds(response.data);
+            }
+        });
+    };
+
+    const onSearch = (event) => {
+        setQuery(event.target.value);
+        fetchQuery();
     }
+
+    const onFilterChange = (event) => {
+        setFilter({...filter, [event.target.name]: event.target.value});
+    }
+
     useEffect(() => {
         fetch();
     }, [])
+
     return (
         <div>
             <div className="text-xl font-bold mb-2">Advertisements</div>
+            <div className="overflow-x-auto relative">
+                <div className="grid gap-6 mb-6 md:grid-cols-2">
+                    <div>
+                        <input 
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            type="text"
+                            value={filter.state}
+                            name="state"
+                            onChange={(event) => onFilterChange(event)}
+                            placeholder="State" 
+                            required
+                        />
+                    </div>
+                    <div>
+                        <input 
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            type="text"
+                            value={filter.city}
+                            name="city"
+                            onChange={(event) => onFilterChange(event)}
+                            placeholder="City" 
+                            required
+                        />
+                    </div>
+                    <div>
+                        <input 
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            type="text"
+                            value={filter.company}
+                            name="company"
+                            onChange={(event) => onFilterChange(event)}
+                            placeholder="Company" 
+                            required
+                        />
+                    </div>
+                    <div>
+                        <input 
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            type="text"
+                            // value={''}
+                            name="tags"
+                            onChange={(event) => onFilterChange(event)}
+                            placeholder="Tags" 
+                            required
+                        />
+                    </div>
+                </div>
+                <button type="button" onClick={() => fetchFilter()}
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 mb-6 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Filter
+                </button>
+                <hr className="mb-6"/>
+                <div className="mb-6">
+                    <input type="text"
+                        value={query}
+                        onChange={(event) => onSearch(event)}
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Title OR Description OR Benefits contains" required/>
+                </div>
+            </div>
             <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
                 <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <thead className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <tr>
-                        <th scope="col" className="py-3 px-6">
+                        <th scope="col" className="py-3 px-3">
+                            Title
+                        </th>
+                        <th scope="col" className="py-3 px-3 break-all">
                             Description
                         </th>
-                        <th scope="col" className="py-3 px-6">
+                        <th scope="col" className="py-3 px-3">
                             Benefits
                         </th>
-                        <th scope="col" className="py-3 px-6">
+                        <th scope="col" className="py-3 px-3">
+                            Salary
+                        </th>
+                        <th scope="col" className="py-3 px-3">
+                            Company
+                        </th>
+                        <th scope="col" className="py-3 px-3">
+                            State
+                        </th>
+                        <th scope="col" className="py-3 px-3">
+                            City
+                        </th>
+                        <th scope="col" className="py-3 px-3">
                             Files
                         </th>
-                        <th scope="col" className="py-3 px-6">
+                        <th scope="col" className="py-3 px-3">
                             Tags
                         </th>
                     </tr>
@@ -39,16 +151,31 @@ function Ads () {
                         ads.map(ad => {
                             return (
                                 <tr key={ad.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                    <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                        {ad.description}
+                                    <th className="py-4 px-3">
+                                        {ad.title}
                                     </th>
-                                    <td className="py-4 px-6">
-                                        {ad.benefits}
+                                    <td className="py-4 px-3">
+                                        {ad.description}
                                     </td>
-                                    <td className="py-4 px-6">
+                                    <td className="py-4 px-3">
+                                        {ad.benefit}
+                                    </td>
+                                    <td className="py-4 px-3">
+                                        {ad.salary}
+                                    </td>
+                                    <td className="py-4 px-3">
+                                        {ad.company}
+                                    </td>
+                                    <td className="py-4 px-3">
+                                        {ad.state}
+                                    </td>
+                                    <td className="py-4 px-3">
+                                        {ad.city}
+                                    </td>
+                                    <td className="py-4 px-3">
                                         {ad.files}
                                     </td>
-                                    <td className="py-4 px-6">
+                                    <td className="py-4 px-3">
                                         {ad.tags}
                                     </td>
                                 </tr>
