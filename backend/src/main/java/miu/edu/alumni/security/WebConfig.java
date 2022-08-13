fe589220ffe71f8703aada2eee5a5c2cd6bf354a
sainturb <saintur.batkhuu@miu.edu>
2022-08-13 10:36:02 -0500

    swagger added

--- backend/src/main/java/miu/edu/alumni/security/WebConfig.java
+++ backend/src/main/java/miu/edu/alumni/security/WebConfig.java
@@ -1,10 +1,24 @@
 package miu.edu.alumni.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
+@EnableSwagger2
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
@@ -12,5 +26,38 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .maxAge(3600);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry
+                .addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .ignoredParameterTypes(AuthenticationPrincipal.class)
+                .select()
+                .paths(PathSelectors.ant("/api/**"))
+                .apis(RequestHandlerSelectors.basePackage("miu.edu.alumni.controller"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "MIU ALUMNI API", //title
+                ".", //description
+                "Version 1.0", //version
+                "Terms of service", //terms of service URL
+                new Contact("Bhanuka Dissanayake", "www.example.com", "myeaddress@company.com"),
+                "License of API", "API license URL", Collections.emptyList()); // contact info
     }
 }
